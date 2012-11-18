6962eaa3e18903d0665a395e407384ba9b53faae
Paul Ser <pashka4281@gmail.com>
2012-11-18 14:04:08 +0200

    surveys index page upd

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -1,2 +1,10 @@
 module SurveysHelper
+
+	def popup_link(name, desc)
+		link_to name, '#', title: desc, rel: 'tooltip'
+	end
+
+	def survey_desc_link(desc)
+		desc.blank? ? popup_link('more', 'There is no description for this survey') : popup_link('more', desc)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Surveyskitchen::Application.routes.draw do
 
-    
-
-  
-
-  
-
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   root to: 'home#index'
