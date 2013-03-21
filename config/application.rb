358543324578f1fd7c75ab9c9fb3b9301e69a5d8
Paul Ser <pashka4281@gmail.com>
2013-03-21 17:43:32 +0200

    asset precompile issue

--- config/application.rb
+++ config/application.rb
@@ -52,7 +52,7 @@ module Surveyskitchen
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += %w(client.css, marketing.css.scss)
+    config.assets.precompile += %w(client.css marketing.css)
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     # Version of your assets, change this if you want to expire all your assets
