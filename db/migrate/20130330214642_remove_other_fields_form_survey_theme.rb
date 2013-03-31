30caa74efedf62e221d99a9e8cd12a6d1f72cb44
Paul Ser <pashka4281@gmail.com>
2013-03-31 14:13:02 +0300

    migration upd

--- db/migrate/20130330214642_remove_other_fields_form_survey_theme.rb
+++ db/migrate/20130330214642_remove_other_fields_form_survey_theme.rb
@@ -1,6 +1,6 @@
 class RemoveOtherFieldsFormSurveyTheme < ActiveRecord::Migration
   def up
-  	remove_column :survey_themes, :survey_title_size
+  	# remove_column :survey_themes, :survey_title_size
   	remove_column :survey_themes, :highlighted_area_color
   	remove_column :survey_themes, :survey_title_font_name
   	remove_column :survey_themes, :item_title_font_name
