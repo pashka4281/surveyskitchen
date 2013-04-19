16c6ec7c12b442b92df52c5daff8cd0c1fc6c7a5
Paul Ser <pashka4281@gmail.com>
2013-04-19 23:20:03 +0300

    blog rights check fix

--- app/controllers/blog/posts_controller.rb
+++ app/controllers/blog/posts_controller.rb
@@ -1,7 +1,7 @@
 class Blog::PostsController < ApplicationController
-	before_filter :authenticate_admin!, only: [:new, :create]
+	before_filter :authenticate_admin!, only: [:new, :create, :edit, :update]
 	skip_before_filter :set_locale
-  	before_filter :set_locale_marketing
+  before_filter :set_locale_marketing
 	
   	inherit_resources
   	defaults :resource_class => Blog::Post, :collection_name => 'posts', :instance_name => 'post' 
